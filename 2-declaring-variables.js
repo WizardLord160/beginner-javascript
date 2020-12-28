600d3fb7a45db9ac7706acdf9be0ce0eb0e81e6e
Dylan Lim <wizardlord160@gmail.com>
2020-12-27 17:08:24 -0800

    initial commit

---
+++ 2-declaring-variables.js
@@ -0,0 +1,13 @@
+var hello = "Hello";
+
+console.log(hello);
+hello = "Hello World";
+console.log(hello);
+
+if (true) {
+    let world = "Hello World";
+    console.log(world);
+}
+
+const dylan = "Dylan";
+dylan = "Dylan Lim"; // this line will return error, cannot reassign
