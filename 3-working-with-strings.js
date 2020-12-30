385138a72c15cd1eda0a67450e5c48da3f635956
Dylan Lim <wizardlord160@gmail.com>
2020-12-29 23:32:44 -0800

    added more lessons

---
+++ 3-working-with-strings.js
@@ -0,0 +1,22 @@
+let str1 = "Hello";
+let str2 = "World!";
+
+//Using the + operator
+console.log("********Using the + operator*******\n");
+console.log(str1 + str2);
+console.log(str1 + "Big + str2");
+
+console.log("********Adding Spacing*******\n");
+//Adding spacing
+str1 = "Hello";
+
+console.log(str1 + str2);
+console.log(str1 + "Big " + str2);
+
+console.log("********Be careful with numbers!*******\n");
+//Be careful with numbers!
+let num1 = 1;
+let num2 = "1";
+
+console.log(num1 + num2);
+console.log(num1 + 1);
